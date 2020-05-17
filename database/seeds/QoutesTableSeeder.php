0d3020a8cab7797cca23bc0e3eb7842ee926dc48
Jeffrey Boisvert <jboisvert377@gmail.com>
2020-05-17 15:12:50 -0400

    Successfully added migrations for users and qoutes.

--- database/seeds/QoutesTableSeeder.php
+++ database/seeds/QoutesTableSeeder.php
@@ -1,45 +1,52 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Qoute;
+use Carbon\Carbon;
+
 
 class QoutesTableSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Used to seed the qoutes table
      *
      * @return void
      */
     public function run()
     {
-        Qoute::create([
+        DB::table('qoutes')->insert([
             'qoute' => 'Don\'t cry because it\'s over, smile because it happened.',
             'author_name' => 'Dr. Seuss',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s')
             ]);	
             
-        Qoute::create([
+        DB::table('qoutes')->insert([
             'qoute' => 'I do not fear computers. I fear lack of them.',
             'author_name' => 'Isaac Asimov',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s')
             ]);	
 
-        Qoute::create([
+        DB::table('qoutes')->insert([
             'qoute' => 'A computer once beat me at chess, but it was no match for me at kick boxing.',
             'author_name' => 'Emo Philips',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s')
             ]);	
 
-        Qoute::create([
+        DB::table('qoutes')->insert([
             'qoute' => 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
             'author_name' => 'Nelson Mandela',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s')
             ]);	
 
-        Qoute::create([
+        DB::table('qoutes')->insert([
             'qoute' => 'Life is what happens when you\'re busy making other plans.',
             'author_name' => 'John Lennon',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s')
             ]);	
 
-        Qoute::create([
+        DB::table('qoutes')->insert([
             'qoute' => 'Always remember that you are absolutely unique. Just like everyone else',
             'author_name' => 'Margaret Mead',
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s')
             ]);	
     }
 }
